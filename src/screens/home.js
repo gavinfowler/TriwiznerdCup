8a23b44a005610d67796c72092312e2b9b1842e8
Gavin Fowler <gavins.username@gmail.com>
2019-03-22 14:13:22 -0600

    created a home page

---
+++ src/screens/home.js
@@ -0,0 +1,46 @@
+/**
+ * Star Wars App
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, View} from 'react-native';
+import { Button, Text, Container } from 'native-base'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class Home extends Component{
+  static navigationOptions = {
+    title: 'Home'
+  }
+
+
+  render() {
+    return (
+      <Container><Button><Text>Test</Text></Button></Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
