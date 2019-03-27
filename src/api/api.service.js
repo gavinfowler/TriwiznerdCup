5cc285c6b18e5ce0bd72570847c988c11a6a50da
Nate <nate.lovell@aggiemail.usu.edu>
2019-03-27 13:41:54 -0600

    Changed getPeople to return list of people models and next URL.

---
+++ src/api/api.service.js
@@ -0,0 +1,37 @@
+let ApiService = class ApiService{
+    constructor() {
+        this.apiProtocol = 'https:';
+        this.apiHost = 'swapi.co/api';
+    }
+
+    get apiLocation() {
+        return `${this.apiProtocol}//${this.apiHost}`;
+    }
+
+    getPeople() {
+        return `${this.apiLocation}/people`;
+    }
+
+    getPlanets {
+        return `${this.apiLocation}/planets`;
+    }
+
+    getFilms {
+        return `${this.apiLocation}/films`;
+    }
+
+    getSpecies {
+        return `${this.apiLocation}/species`;
+    }
+
+    getVehicles {
+        return `${this.apiLocation}/vehicles`;
+    }
+
+    getStarships {
+        return `${this.apiLocation}/starships`;
+    }
+};
+
+const apiService = new ApiService();
+export default apiService;
---
+++ src/api/swapi.service.js
@@ -0,0 +1,38 @@
+import ApiService from './api.service';
+import {people} from '../models/peopleModel';
+
+let SwapiService = class SwapiService {
+    constructor() {
+    }
+
+    getPeople(url=ApiService.getPeople()) {
+        //if (url == '') {
+        //    url = ApiService.getPeople();
+        //}
+
+        //we actually only need names and URLs.
+        return new Promise((resolve, reject) => {
+            fetch(url)
+            .then((response) => response.json())
+            .then((response) => {
+                let items = [];
+                response.results.forEach(person => {
+                    //items.push(new People(stuff here))
+                    items.push(new people(person));
+                });
+                let result = {
+                    items: items,
+                    next: response.next
+                };
+                resolve(result);
+            });
+            .catch((error) => {
+                console.error(error);
+                reject(error);
+            });
+        });
+    }
+};
+
+let swapiService = new SwapiService();
+export default swapiService;
